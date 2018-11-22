dd98f07ed8edca999523b0952d6c4a2e7b108237
Rilong <romgnatyuk@gmail.com>
2018-11-22 11:11:17 +0200

    Included Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase'
+
+export default firebase.initializeApp({
+  apiKey: 'AIzaSyBEws4fbP-6aAt3VAXFAmcWR-8_ZrgJrDo',
+  authDomain: 'workplaningpro.firebaseapp.com',
+  databaseURL: 'https://workplaningpro.firebaseio.com',
+  projectId: 'workplaningpro',
+  storageBucket: 'workplaningpro.appspot.com',
+  messagingSenderId: '855410034243',
+})
