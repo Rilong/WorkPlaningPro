83b7c36906206d7fb82aeeab71afeb6c8d468a3a
Rilong <romgnatyuk@gmail.com>
2019-01-10 11:35:15 +0200

    Add current month label

--- src/components/ui/calendar/Calendar.tsx
+++ src/components/ui/calendar/Calendar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
-// import * as dateFns from 'date-fns'
-import {Fab, Grid} from "@material-ui/core";
+import * as dateFns from 'date-fns'
+import * as ru from 'date-fns/locale/ru'
+import {Fab, Grid, Typography} from "@material-ui/core";
 import ArrowBack from "@material-ui/icons/ArrowBack"
 import ArrowForward from "@material-ui/icons/ArrowForward"
 
@@ -20,21 +21,35 @@ class Calendar extends React.Component<IProps, IState> {
     selectedDate: new Date()
   }
 
+  private locale = ru;
+
   constructor(props: IProps) {
     super(props)
   }
 
+  public nextMonth = () => {
+    console.log("Next month");
+  }
+
+  public prevMonth = () => {
+    console.log("Previous month");
+  }
+
+
   public headerRender() {
+    const currentMonth = dateFns.format(this.state.currentDate, 'MMMM', {
+      locale: this.locale
+    });
     return (
       <>
         <Grid container={true} item={true} xs={3} justify="flex-start">
-          <Fab color="secondary"><ArrowBack/></Fab>
+          <Fab color="secondary" onClick={this.prevMonth}><ArrowBack/></Fab>
         </Grid>
         <Grid container={true} item={true} xs={6} justify="center">
-          test
+          <Typography variant="h5">{currentMonth}</Typography>
         </Grid>
         <Grid container={true} item={true} xs={3} justify="flex-end">
-          <Fab color="secondary"><ArrowForward/></Fab>
+          <Fab color="secondary" onClick={this.nextMonth}><ArrowForward/></Fab>
         </Grid>
       </>
     )
