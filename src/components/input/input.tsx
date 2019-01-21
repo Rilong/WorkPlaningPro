c76941d9b7d80f989aca410613da641f49a40033
Rilong <romgnatyuk@gmail.com>
2019-01-21 11:47:23 +0200

    Add variant to props

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,8 +1,7 @@
-import InputLabel from '@material-ui/core/InputLabel/InputLabel';
-import Input from '@material-ui/core/Input/Input';
 import FormControl from '@material-ui/core/FormControl/FormControl';
 import * as React from 'react';
 import FormHelperText from '@material-ui/core/FormHelperText/FormHelperText';
+import {TextField} from "@material-ui/core";
 
 interface IProps {
   value: string
@@ -13,15 +12,19 @@ interface IProps {
   errorMessage?: string
   change?: (e: any) => void,
   className?: string
+  variant?: "standard" | "outlined" | "filled"
 }
 
 export default (props: IProps) => {
   const errorId = `${props.name}__error`
-
   return (
     <FormControl fullWidth={true} error={props.hasError} aria-describedby={errorId} className={props.className}>
-      <InputLabel htmlFor={props.name}>{props.label}</InputLabel>
-      <Input id={props.name} type={props.type} value={props.value} onChange={props.change}/>
+      <TextField id={props.name}
+                 type={props.type}
+                 value={props.value}
+                 onChange={props.change}
+                 label={props.label}
+                 variant={typeof props.variant === 'undefined' ? 'standard' : props.variant as any}/>
       {props.hasError ? <FormHelperText id={errorId}>{props.errorMessage}</FormHelperText> : null}
     </FormControl>
   );
