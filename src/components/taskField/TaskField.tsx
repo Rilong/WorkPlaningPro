52773d0c3a9a622f358575a0ec5f82c54b28ef04
Rilong <romgnatyuk@gmail.com>
2019-01-21 10:28:05 +0200

    Auto focus outside

--- src/components/taskField/TaskField.tsx
+++ src/components/taskField/TaskField.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {Typography, TextField} from '@material-ui/core'
 
 interface IProps {
+  autofocus?: boolean
   value: string
   change?: (value: string) => void
 }
@@ -16,6 +17,10 @@ class TaskField extends React.Component<IProps, IState> {
 
   public state = {
     inputShowed: true,
+  }
+
+  public static defaultProps = {
+    autofocus: false
   }
 
   private focusLost = () => {
@@ -40,7 +45,7 @@ class TaskField extends React.Component<IProps, IState> {
         {this.state.inputShowed !== true
          ? <Typography variant="body1" onClick={this.onFocusHandler} style={{cursor: 'pointer'}}>{this.props.value}</Typography>
          : <TextField placeholder="Введите название задачи"
-                      autoFocus={true}
+                      autoFocus={this.props.autofocus}
                       value={this.props.value || ''}
                       onChange={(e: any) => this.props.change(e.target.value)}
                       inputRef={this.input}
