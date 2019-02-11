c0f1aac55322879a0b4b91eae58315db940f7636
Rilong <romgnatyuk@gmail.com>
2019-02-11 13:53:57 +0200

    Made show controls on hover

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -17,30 +17,48 @@ interface IProps {
   classes?: IStyles
 }
 
+interface IState {
+  showControls: boolean
+}
 
-const Task = (props: IProps) => (
-  <Card className={props.classes.task}>
-    <div className={props.classes.controls}>
-      {typeof props.checked !== 'undefined'
-        ? <Checkbox color="primary"
-                    classes={{root: props.classes.checkRoot}}
-                    checked={props.checked}
-                    onChange={props.onCheckChange}
-                    disabled={typeof props.checkDisable === 'undefined' ? false : props.checkDisable}/> : null}
+class Task extends React.Component<IProps, IState> {
+  public state = {
+    showControls: false
+  }
 
-      <Grid container={true} alignItems="center" justify="space-between">
-        <Grid item={true} xs={10} style={{paddingRight: '10px'}}>
-          <TaskField value={props.value}
-                     change={props.onChange}/>
-          <Typography variant="body2" className={props.classes.dateText}>20.02.2019</Typography>
-        </Grid>
-        <Grid item={true} xs={2}>
-          <Fab color="primary" size="small" style={{marginRight: '10px'}} onClick={props.onDatelinePicker}><CalendarIcon/></Fab>
-          <Fab color="primary" size="small" onClick={props.onRemove}><CloseIcon/></Fab>
-        </Grid>
-      </Grid>
-    </div>
-  </Card>
-)
+  private cardEnter = () => this.setState({showControls: true})
+  private cardLeave = () => this.setState({showControls: false})
+
+  public render(): React.ReactNode {
+    return (
+      <Card className={this.props.classes.task} onMouseEnter={this.cardEnter} onMouseLeave={this.cardLeave}>
+        <div className={this.props.classes.controls}>
+          {typeof this.props.checked !== 'undefined'
+            ? <Checkbox color="primary"
+                        classes={{root: this.props.classes.checkRoot}}
+                        checked={this.props.checked}
+                        onChange={this.props.onCheckChange}
+                        disabled={typeof this.props.checkDisable === 'undefined' ? false : this.props.checkDisable}/> : null}
+
+          <Grid container={true} alignItems="center" justify="space-between">
+            <Grid item={true} xs={10} style={{paddingRight: '10px'}}>
+              <TaskField value={this.props.value}
+                         change={this.props.onChange}/>
+              <Typography variant="body2" className={this.props.classes.dateText}>20.02.2019</Typography>
+            </Grid>
+            <Grid item={true} xs={2} className={this.props.classes.controlsRoot}>
+              {this.state.showControls ?
+                  <>
+                    <Fab color="primary" size="small" style={{marginRight: '10px'}} onClick={this.props.onDatelinePicker}><CalendarIcon/></Fab>
+                    <Fab color="primary" size="small" onClick={this.props.onRemove}><CloseIcon/></Fab>
+                  </> : null
+              }
+            </Grid>
+          </Grid>
+        </div>
+      </Card>
+    )
+  }
+}
 
 export default withStyles(styles)(Task)
--- src/components/task/styles.tsx
+++ src/components/task/styles.tsx
@@ -3,6 +3,7 @@ import createStyles from "@material-ui/core/styles/createStyles";
 export interface IStyles {
   task: string
   checkRoot: string
+  controlsRoot: string
   controls: string
   dateText: string
 }
@@ -15,6 +16,9 @@ export const styles = createStyles({
   checkRoot: {
     padding: '0',
     marginRight: '10px'
+  },
+  controlsRoot: {
+    textAlign: 'right'
   },
   controls: {
     display: 'flex',
