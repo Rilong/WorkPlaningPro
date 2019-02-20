e0f646d31fb89676ba56bd83c7b3eca5fd937cfe
Rilong <romgnatyuk@gmail.com>
2019-02-20 16:55:53 +0200

    Add new project

--- src/store/reducers/projectListReducer.ts
+++ src/store/reducers/projectListReducer.ts
@@ -1,6 +1,6 @@
 import {IAction} from '../../interfaces/IAction'
 import {IProjectListState} from '../../interfaces/projects/IProjectListState'
-import {PROJECTS_GET} from '../actions/project-list/actionTypes'
+import {PROJECTS_ADD, PROJECTS_GET} from '../actions/project-list/actionTypes'
 
 const initialState: IProjectListState = {
   projects: [],
@@ -11,6 +11,10 @@ export default function (state: IProjectListState = initialState, action: IActio
   switch (action.type) {
     case PROJECTS_GET:
       return {...state, projects: action.payload}
+    case PROJECTS_ADD:
+      const projects = [...state.projects]
+      projects.push(action.payload)
+      return {...state, projects}
     default:
       return state
   }
