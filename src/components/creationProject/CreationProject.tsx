6c73858622e9b38ed312381c4f246376e1881309
Rilong <romgnatyuk@gmail.com>
2019-02-21 15:21:26 +0200

    Moved creation project logic to a new component

---
+++ src/components/creationProject/CreationProject.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react'
+import {Fab, Grid, TextField} from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import DialogAction from '../DialogAction/DialogAction'
+import {createProject} from '../../store/actions/project/actions'
+import {connect} from 'react-redux'
+
+interface IProps {
+  onCreateProject?: (projectName: string) => Promise<void>
+  loading?: boolean
+}
+
+interface IState {
+  createProjectValue: string
+  createProjectValid: boolean
+  createProjectOpen: boolean
+}
+
+class CreationProject extends React.Component<IProps, IState> {
+  public state = {
+    createProjectValue: '',
+    createProjectValid: false,
+    createProjectOpen: false
+  }
+
+  private projectNameRef = React.createRef<HTMLInputElement>()
+
+  private onCreateProjectEntered = () => this.projectNameRef.current.focus()
+
+  private createProjectOpen = () => this.setState({createProjectOpen: true})
+
+  private createProjectClose = () => {
+    this.setState({
+      createProjectOpen: false,
+      createProjectValue: '',
+      createProjectValid: false,
+    })
+  }
+
+  private onProjectNameChangeHandler = (value: string) => {
+    this.setState({createProjectValue: value})
+
+    if (value.trim().length !== 0 && this.state.createProjectValid === false) {
+      this.setState({createProjectValid: true})
+    } else if (value.trim().length === 0 && this.state.createProjectValid === true) {
+      this.setState({createProjectValid: false})
+    }
+  }
+
+  private createProject = () => {
+    if (this.state.createProjectValid) {
+      this.props.onCreateProject(this.state.createProjectValue).then(() => this.createProjectClose())
+    }
+  }
+
+  private createProjectOnEnter(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      this.createProject()
+    }
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <div>
+        <Grid container={true} justify="flex-end" classes={{container: 'homeContainer'}}>
+          <Fab color="primary" onClick={this.createProjectOpen}><AddIcon/></Fab>
+        </Grid>
+        <DialogAction open={this.state.createProjectOpen}
+                      onEntered={this.onCreateProjectEntered}
+                      onAgree={this.createProject}
+                      onClose={this.createProjectClose}
+                      title="Добаления проекта"
+                      agreeLabel="Создать проект"
+                      disabled={!this.state.createProjectValid || this.props.loading}
+                      loading={this.props.loading}
+                      onDisagree={this.createProjectClose}
+        >
+          <TextField placeholder="Ввейдите имя проекта"
+                     inputRef={this.projectNameRef}
+                     value={this.state.createProjectValue}
+                     onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) => this.createProjectOnEnter(event)}
+                     onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.onProjectNameChangeHandler(event.target.value)}
+                     fullWidth={true}/>
+        </DialogAction>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state: any) {
+  return {
+    createProjectOpen: state.ProjectReducer.open,
+    loading: state.ProjectReducer.loading,
+  }
+}
+
+function mapDispatchToProps(dispatch: any) {
+  return {
+    onCreateProject: (projectName: string) => dispatch(createProject(projectName))
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreationProject)
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,72 +1,21 @@
 import * as React from 'react'
 import {connect} from 'react-redux'
-import {Grid, Fab, TextField} from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add'
-import DialogAction from '../../components/DialogAction/DialogAction'
+import {Grid} from '@material-ui/core'
 import ProjectCard from '../../components/projectCard/ProjectCard'
-import {createProject} from '../../store/actions/project/actions'
 
 import './styles.scss'
 import {IProject} from '../../interfaces/projects/IProject'
 import {GridSpacing} from '@material-ui/core/Grid'
 import {Link} from 'react-router-dom'
+import CreationProject from '../../components/creationProject/CreationProject'
 
 interface IProps {
   createProjectOpen: boolean
-  onCreateProject?: (projectName: string) => Promise<void>
   projects?: IProject[]
-  loading?: boolean
 }
 
-interface IState {
-  createProjectValue: string
-  createProjectValid: boolean
-  createProjectOpen: boolean
-}
 
-class Home extends React.Component<IProps, IState> {
-
-  public state = {
-    createProjectValue: '',
-    createProjectValid: false,
-    createProjectOpen: false
-  }
-
-  private projectNameRef = React.createRef<HTMLInputElement>()
-
-  private onCreateProjectEntered = () => this.projectNameRef.current.focus()
-
-  private createProjectOpen = () => this.setState({createProjectOpen: true})
-
-  private createProjectClose = () => {
-    this.setState({
-      createProjectOpen: false,
-      createProjectValue: '',
-      createProjectValid: false,
-    })
-  }
-
-  private onProjectNameChangeHandler = (value: string) => {
-    this.setState({createProjectValue: value})
-
-    if (value.trim().length !== 0 && this.state.createProjectValid === false) {
-      this.setState({createProjectValid: true})
-    } else if (value.trim().length === 0 && this.state.createProjectValid === true) {
-      this.setState({createProjectValid: false})
-    }
-  }
-
-  private createProject = () => {
-    if (this.state.createProjectValid) {
-      this.props.onCreateProject(this.state.createProjectValue).then(() => this.createProjectClose())
-    }
-  }
-
-  private createProjectOnEnter(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter') {
-      this.createProject()
-    }
-  }
+class Home extends React.Component<IProps> {
 
   private projectsList() {
     return (
@@ -88,29 +37,8 @@ class Home extends React.Component<IProps, IState> {
   public render() {
     return (
       <div>
-        <Grid container={true} justify="flex-end" classes={{container: 'homeContainer'}}>
-          <Fab color="primary" onClick={this.createProjectOpen}><AddIcon/></Fab>
-        </Grid>
-        <DialogAction open={this.state.createProjectOpen}
-                      onEntered={this.onCreateProjectEntered}
-                      onAgree={this.createProject}
-                      onClose={this.createProjectClose}
-                      title="Добаления проекта"
-                      agreeLabel="Создать проект"
-                      disabled={!this.state.createProjectValid || this.props.loading}
-                      loading={this.props.loading}
-                      onDisagree={this.createProjectClose}
-        >
-          <TextField placeholder="Ввейдите имя проекта"
-                     inputRef={this.projectNameRef}
-                     value={this.state.createProjectValue}
-                     onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) => this.createProjectOnEnter(event)}
-                     onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.onProjectNameChangeHandler(event.target.value)}
-                     fullWidth={true}/>
-        </DialogAction>
-
+        <CreationProject/>
         {this.projectsList()}
-
       </div>
     )
   }
@@ -118,16 +46,8 @@ class Home extends React.Component<IProps, IState> {
 
 function mapStateToProps(state: any) {
   return {
-    createProjectOpen: state.ProjectReducer.open,
-    loading: state.ProjectReducer.loading,
     projects: state.ProjectListReducer.projects
   }
 }
 
-function mapDispatchToProps(dispatch: any) {
-  return {
-    onCreateProject: (projectName: string) => dispatch(createProject(projectName))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps)(Home)
