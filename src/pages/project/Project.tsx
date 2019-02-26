a44c2bd78038d5d326eb3f43ab17adf665b9a229
Rilong <romgnatyuk@gmail.com>
2019-02-26 16:17:04 +0200

    UI for project page 6/9

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import {Typography} from '@material-ui/core'
+import {Typography, Grid, Card, CardContent, LinearProgress, Fab, Divider} from '@material-ui/core'
+import CalendarIcon from '@material-ui/icons/Today'
+import MoneyIcon from '@material-ui/icons/MonetizationOn'
 import {match, withRouter} from 'react-router'
+import './styles.scss'
+import Task from '../../components/tasks/task/Task'
 
 interface IParams {
   id: string
@@ -16,7 +20,42 @@ class Project extends React.Component<IProps> {
 
     return (
       <div>
-        <Typography variant="h2">Project page - {this.props.match.params.id}</Typography>
+        <Grid container={true} justify="center" className="pjContainer">
+          <Grid item={true} xs={5}>
+            <Card>
+              <CardContent>
+                <Typography variant="h4" className="pjName">Test</Typography>
+                <Grid container={true} justify="space-between">
+                  <Typography variant="subtitle2">
+                    <Fab size="small" color="primary" className="pjDateStartBtn"><CalendarIcon/></Fab>
+                    Дата начала 26.02.2019
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Дата завершения 05.03.2019
+                    <Fab size="small" color="primary" className="pjDateFinishBtn"><CalendarIcon/></Fab>
+                  </Typography>
+                </Grid>
+              </CardContent>
+              <Grid container={true} className="pjInfo">
+                <LinearProgress variant="determinate" value={67} className="pjProgress"/>
+                <Grid container={true} justify="space-between">
+                  <Typography variant="body1" className="pjMoneyText"><MoneyIcon/> <span>0</span></Typography>
+                  <Typography variant="body1" className="pjProgressText"><span>67%</span></Typography>
+                </Grid>
+              </Grid>
+              <Divider/>
+              <CardContent>
+                <Typography variant="h6" align="center">Задачи</Typography>
+                <div className="pjTasks">
+                  <Task value="Тестовая задача" checked={true} className="pjTask"/>
+                  <Task value="Тестовая задача" checked={true} className="pjTask"/>
+                  <Task value="Тестовая задача" checked={false} className="pjTask"/>
+                  <Task value="Тестовая задача" checked={false} className="pjTask"/>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
       </div>
     )
   }
